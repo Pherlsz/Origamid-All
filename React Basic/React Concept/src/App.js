4278c86f62bbfde6928a57de7416a7363f2bcd14
Pedro Henrique Lopes <38593854+Pherlsz@users.noreply.github.com>
2022-11-13 23:27:08 -0300

    React basic concept course

---
+++ React Basic/React Concept/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return React.createElement(
+    "a",
+    { href: "https://www.origamid.com" },
+    "Origamid"
+  );
+};
+
+export default App;
