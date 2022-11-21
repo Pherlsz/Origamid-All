085977c54f739350c4cd13e78c6aa810ef1660bb
Pedro Henrique Lopes <38593854+Pherlsz@users.noreply.github.com>
2022-11-20 22:06:31 -0300

    Create react app - exercicio 1

---
+++ React Basic/Create React App/Exercicio 1/src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const luana = {
+  nome: 'Luana',
+  idade: 27,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+  ],
+  ativa: true,
+};
+
+const mario = {
+  nome: 'Mario',
+  idade: 31,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+    { nome: 'Guitarra', preco: 'R$ 3500' },
+  ],
+  ativa: false,
+};
+
+const App = () => {
+  const dados = luana;
+  const total = dados.compras
+    .map((item) => Number(item.preco.replace('R$ ', '')))
+    .reduce((a, b) => a + b);
+
+  return (
+    <>
+      <p>Nome: {dados.nome}</p>
+      <p>Idade: {dados.idade}</p>
+      <p>
+        Situação:{' '}
+        <span style={{ color: dados.ativa ? 'green' : 'red' }}>
+          {dados.ativa ? 'Ativa' : 'Inativa'}
+        </span>
+      </p>
+      <p>
+        Total gasto:{' '}
+        {/* if total > 10000 let the total color red, if not green */}
+        <span style={{ color: total > 10000 ? 'red' : 'green' }}>
+          R${total}
+        </span>
+      </p>
+      {total > 10000 && <p>Você está gastando muito</p>}
+    </>
+  );
+};
+
+export default App;
